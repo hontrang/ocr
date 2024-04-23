c8a142d68fe5db46e8510e16452176b62c8d9d4d
hon.trang <hon.trang@isobar.com>
2024-04-23 17:58:30 +0700

    refactor: change method to check code updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,13 +9,15 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.time.LocalDateTime;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException, AWTException {
-        DriveUtility drive;
         String credentialsFilePath = "icauto_asics.json";
         String fileName = "code.txt";
+        String code = "";
         FileUtility fileUtility = new FileUtility();
 
         int x = 0;
@@ -43,23 +45,50 @@ public class Main {
             }
         }
         while (true) {
-            CaptureScreenUtility.captureScreen(x, y, width, height, screenshot);
-            fileUtility.writeFile(fileName, OCRUtility.doOCR(screenshot));
-            drive = new DriveUtility(credentialsFilePath);
-            try {
-                drive.updateFile(fileName, System.getProperty("user.dir") + java.io.File.separator + fileName, "text/plain text");
-            } catch (SocketTimeoutException | SSLException | SocketException | GoogleJsonResponseException e) {
-                e.printStackTrace();
+            if (scanUntilImageChanged(fileName, screenshot, x, y, width, height)) {
+                code = OCRUtility.doOCR(screenshot);
+                fileUtility.writeFile(fileName, code);
+                pushFileToGoogleDrive(credentialsFilePath, fileName);
+                sleep(29000);
             }
-            sleep(2000);
+
         }
     }
 
-    private static void sleep(int timeInSeconds) {
+    private static void sleep(long timeInMiliSeconds) {
         try {
-            Thread.sleep(timeInSeconds);
+            Thread.sleep(timeInMiliSeconds);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getFirstLine(String fileName) {
+        FileUtility fileUtility = new FileUtility();
+        try {
+            return fileUtility.readFile(fileName).get(0);
+        } catch (IndexOutOfBoundsException e) {
+            return "";
+        }
+    }
+
+    private static void pushFileToGoogleDrive(String credentialsFilePath, String fileName) throws IOException, AWTException {
+        DriveUtility drive = new DriveUtility(credentialsFilePath);
+        try {
+            drive.updateFile(fileName, System.getProperty("user.dir") + java.io.File.separator + fileName, "text/plain text");
+        } catch (SocketTimeoutException | SSLException | SocketException | GoogleJsonResponseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean scanUntilImageChanged(String fileName, String screenshot, int x, int y, int width, int height) throws IOException, AWTException {
+        String code = OCRUtility.doOCR(screenshot);
+        String currentCode = getFirstLine(fileName);
+        while (code.equals(currentCode)) {
+            CaptureScreenUtility.captureScreen(x, y, width, height, screenshot);
+            code = OCRUtility.doOCR(screenshot);
+            sleep(100);
+        }
+        return true;
     }
 }
